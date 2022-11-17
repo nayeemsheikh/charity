229ce81813e0c829a6568aebe9209821678997bc
front end developer <56119725+nayeemsheikh@users.noreply.github.com>
2022-11-17 13:08:34 +0800

    added some file

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php  
+    // document open
+    include './partials/document-open.php'; 
+?>
+
+<section class="banner banner--demo">
+    <div class="overlay">
+        <div class="container text-center text-white">
+            <picture>
+                <img class="banner__img" src="./images/logos/logo.webp" alt="site-logo">
+            </picture>
+            <h1 class="banner__title">CharityPress</h1>
+            <div class="banner__group-btn">
+                <a class="banner__primary-btn" href="#">buy theme</a>  
+                <a class="banner__secondary-btn" href="#">demos</a>  
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include './partials/document.close.php' ?>
